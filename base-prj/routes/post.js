07a94752d2c5c378cb95f77ce5afeb1c105b51c8
juhye <yoyoung675@gmail.com>
2022-05-21 18:11:35 +0900

    juhye unfollow and delete

--- base-prj/routes/post.js
+++ base-prj/routes/post.js
@@ -64,7 +64,38 @@ router.post('/', isLoggedIn, upload2.none(), async (req, res, next) => {
   }
 });
 
-router.delete('/:id', isLoggedIn, async (req, res, next) => {
+// router.delete('/:id', isLoggedIn, async (req, res, next) => {
+//     try {
+//       const result = await Post.update({
+//         flag: false,
+//         deletedAt: new Date(),
+//       }, {
+//         where: { id: req.params.id },
+//       });
+//       res.json(result);
+//     } catch (err) {
+//       console.error(err);
+//       next(err);
+//     }
+// });
+
+router.route('/:id')
+  .patch(async (req, res, next) => {
+    try {
+      const result = await Post.update({
+        comment: req.body.comment,
+        img: req.body.url,
+        updatedAt: new Date(),
+      }, {
+        where: { id: req.params.id },
+      });
+      res.json(result);
+    } catch (err) {
+      console.error(err);
+      next(err);
+    }
+  })
+  .delete('/:id', isLoggedIn, async (req, res, next) => {
     try {
       const result = await Post.update({
         flag: false,
@@ -77,6 +108,6 @@ router.delete('/:id', isLoggedIn, async (req, res, next) => {
       console.error(err);
       next(err);
     }
-});
+  });
 
 module.exports = router;
