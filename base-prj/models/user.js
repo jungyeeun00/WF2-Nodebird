760ea67a0e6eb09d154d124bb955fe1378b8ad6b
jungyeeun00 <74102394+jungyeeun00@users.noreply.github.com>
2022-05-23 08:06:05 +0900

    add twit like function

--- base-prj/models/user.js
+++ base-prj/models/user.js
@@ -54,6 +54,6 @@ module.exports = class User extends Sequelize.Model {
       as: 'Followings',
       through: 'Follow',
     });
-    db.User.belongsToMany(db.Post, { through:'Like' })
+    db.User.belongsToMany(db.Post, { through:'Like', as:'Likes' })
   }
 };
